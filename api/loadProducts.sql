a08ff9a1ebd969f44cef39c70364dfa70dcfeb92
silverbryan <brayanpbarraza@gmail.com>
2020-10-08 18:37:05 -0300

    update.sql

--- api/loadProducts.sql
+++ api/loadProducts.sql
@@ -1,2 +1,2 @@
-INSERT INTO products(name, stock, description, price, enter_date, image)
-VALUES('Hola',23,'Hola',4030,2016-06-22 19:10:25-07,'fjdjfd.jpg');
+INSERT INTO products(name, stock, description, price, image)
+VALUES('Hola',23,'Hola',4000,'fjdjfd.jpg');
